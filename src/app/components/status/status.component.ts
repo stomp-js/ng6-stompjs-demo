18855b78584955b3e947157e4d63e462c98935d0
deepak <deepak@kreatio.com>
2018-07-14 12:15:33 +0530

    Working sample demonstrating disconnect, retires, and reconnect

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,5 +1,5 @@
 
-import {map} from 'rxjs/operators';
+import {filter, map} from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -21,6 +21,35 @@ export class StatusComponent implements OnInit {
   ngOnInit() {
     console.log('Status init');
     this.state = this._stompService.state.pipe(
-      map((state: number) => StompState[state]));
+      map((state: number) => {
+        console.log(`Current state: ${StompState[state]}`);
+        return StompState[state];
+      })
+    );
+
+    const MAX_RETRIES = 3;
+    let numRetries = MAX_RETRIES;
+
+    this._stompService.state.pipe(
+      filter((state: number) => state === StompState.CLOSED)
+    ).subscribe(() => {
+      console.log(`Will retry ${numRetries} times`);
+      if (numRetries <= 0) {
+        this._stompService.disconnect();
+      }
+      numRetries--;
+    });
+
+    this._stompService.connectObservable.subscribe(() => {
+      numRetries = MAX_RETRIES;
+    });
+  }
+
+  connect() {
+    this._stompService.initAndConnect();
+  }
+
+  disconnect() {
+    this._stompService.disconnect();
   }
 }
